f828b3b9fc2c87e4a6698cd03c6637b85503727f
HadertS <source.zero@googlemail.com>
2024-04-05 19:49:37 +0100

    fixed resolution scaling and paddle animation

--- entitites/paddle/paddle.cs
+++ entitites/paddle/paddle.cs
@@ -4,6 +4,12 @@ using System;
 public partial class paddle : CharacterBody2D
 {
 	private int speed = 800;
+    private AnimatedSprite2D animatedSprite;
+    public override void _Ready()
+    {   
+        animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        animatedSprite.Play("default");    
+    }
 
     public override void _PhysicsProcess(double delta)
     {
