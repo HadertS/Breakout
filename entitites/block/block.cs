dc03ae35a538c2a87482184c2a528fe69b3dcb67
HadertS <source.zero@googlemail.com>
2024-05-30 11:51:43 +0100

    rewrote block animation handling and fixed some alignment on pause menu

--- entitites/block/block.cs
+++ entitites/block/block.cs
@@ -5,55 +5,26 @@ public partial class block : Node
 {
 	[Export]
 	private int Hitpoints { get; set; } = 1;
+	AnimatedSprite2D animatedSprite2D;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		switch (Hitpoints){
-			case 1:
-				animatedSprite2D.Play("red");
-				animatedSprite2D.SetFrameAndProgress(1,1);
-			break;
-			case 2:
-				animatedSprite2D.Play("red");
-				animatedSprite2D.SetFrameAndProgress(0,1);
-			break;
-			case 3:
-				animatedSprite2D.Play("blue_dark");
-				animatedSprite2D.SetFrameAndProgress(1,1);
-			break;
-			case 4:
-				animatedSprite2D.Play("blue_dark");
-				animatedSprite2D.SetFrameAndProgress(0,1);
-
-			break;
-			default:
-				animatedSprite2D.Play("grey");
-			break;
-		}
+		animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play("default");
+		animatedSprite2D.SetFrameAndProgress(Hitpoints - 1,1);
 		animatedSprite2D.Pause();
 	}
 
 	private void OnHit(){
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		Hitpoints--;
-
-		switch(Hitpoints){
-			case 0:
-				QueueFree();
-			break;
-			case 1:
-				animatedSprite2D.SetFrameAndProgress(1,1);
-			break;
-			case 2:
-				animatedSprite2D.Play("red");
-				animatedSprite2D.SetFrameAndProgress(0,1);
-			break;
-			case 3:
-				animatedSprite2D.SetFrameAndProgress(1,1);
-			break;
+		if (Hitpoints <= 0){
+			QueueFree();
 		}
+		else{
+			animatedSprite2D.SetFrameAndProgress(Hitpoints - 1,1);
+		}
+
 		animatedSprite2D.Pause();
 
 	}
