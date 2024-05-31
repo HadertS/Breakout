c1309c88d24299b14806739bac43469bc3a574df
HadertS <source.zero@googlemail.com>
2024-05-31 12:27:28 +0100

    Added powerup entity and blocks can now drop powerups, changed collision groups and masks.

--- Entities/Block/Block.cs
+++ Entities/Block/Block.cs
@@ -1,10 +1,12 @@
-using Godot;
 using System;
+using Godot;
 
-public partial class Block : Node
+public partial class Block : Node2D
 {
 	[Export]
 	private int Hitpoints { get; set; } = 1;
+	[Export(PropertyHint.Enum, "None, PaddleSize, BallSize")]
+	public String PowerUpType { get; set; } = "None";
 	AnimatedSprite2D animatedSprite2D;
 
 	// Called when the node enters the scene tree for the first time.
@@ -19,6 +21,14 @@ public partial class Block : Node
 	private void OnHit(){
 		Hitpoints--;
 		if (Hitpoints <= 0){
+			if (PowerUpType != "None"){
+				PackedScene powerUpScene = GD.Load<PackedScene>("res://Entities/PowerUp/PowerUp.tscn");
+				PowerUp powerUpInstance = powerUpScene.Instantiate() as PowerUp;
+				GetParent().AddChild(powerUpInstance);
+				powerUpInstance.Position = Position;
+				powerUpInstance.PowerUpType = PowerUpType;
+			}
+			
 			QueueFree();
 		}
 		else{
